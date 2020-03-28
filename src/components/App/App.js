e7576e02bc9fb70e7505a9e3186132408c32e13f
dembrabus <danmontoya001@gmail.com>
2020-03-27 22:47:14 -0400

    initial commit, My Program is birthed
    git status
    Qœœ
    
    clear

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App" data-react="react">
+      <h1>Hello there from my awesome app</h1>
+    </div>
+  );
+}
+
+export default App;
